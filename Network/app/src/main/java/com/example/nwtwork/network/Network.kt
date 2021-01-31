4e87b1d19b3b3388faadbf585048e674e7d5b9b0
vadim <ivadimn@mail.ru>
2021-01-31 13:37:12 +0300

    new version Network

--- Network/app/src/main/java/com/example/nwtwork/network/Network.kt
+++ Network/app/src/main/java/com/example/nwtwork/network/Network.kt
@@ -1,15 +1,26 @@
 package com.example.nwtwork.network
 
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import okhttp3.Call
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 
 object Network {
 
     private val MOVIE_API_KEY = "2e0fc156"
 
-    val client = OkHttpClient()
+    val flipperNetworkPlugin = NetworkFlipperPlugin()
+
+    val client = OkHttpClient.Builder()
+            .addNetworkInterceptor(CustomHeaderInterceptor("Header1", "Value1"))
+            .addNetworkInterceptor(CustomHeaderInterceptor("Header2", "Value2"))
+            .addNetworkInterceptor(HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY))
+        .addNetworkInterceptor(FlipperOkhttpInterceptor(flipperNetworkPlugin))
+            .build()
 
     fun getSearchMovieCall(text : String) : Call {
         val url = HttpUrl.Builder()
